6febf80dd0fc7069221bf702df363b3056b98eff
pranava muthu <103396008+pranava007@users.noreply.github.com>
2024-02-26 17:23:36 +0530

    Create HollowSquare.c

---
+++ HollowSquare.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+
+int n=5;
+
+   for(int i=0;i<=n;i++){
+      
+       for(int j=0;j<=n;j++){
+          
+          if(i==0||i==n||j==0||j==n){
+              printf("* ");
+          }
+          else{
+              printf("  ");
+          }
+           
+       }
+
+       printf("\n");
+
+}
+
+}
